11d43f9d6b760085ebd68405e5cdbf09b5ba02ef
Emanuel Hernandez <emanuelhernandez@students.berkeley.net>
2019-02-13 16:18:45 -0800

    yeniyeni

---
+++ src/Giraffe.java
@@ -0,0 +1,23 @@
+public class Giraffe extends Animal{
+
+    public Giraffe(String name){
+        super(name, "leaves");
+    }
+
+    public void eat(String food){
+
+        System.out.println(this.name + " eats " + food);
+
+        if( food == favoriteFood){
+
+            System.out.println("YUM!!! "+ this.name + " wants more " + food);
+            sleep();
+
+        }else{
+
+            System.out.println("YUCK!!! " + this.name + " will not eat " + food);
+
+        }
+
+    }
+}
